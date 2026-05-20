1fb500973df227accf0580746fd85f3a8c6eb404
S Janakiraman <s.janakiraman@ericsson.com>
2026-05-20 07:29:29 +0530

    Initial commit 20 June 2026

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center content-center gap-4">
+      <h1>Made with by Janakiraman S</h1>
+      </div>
+  );
+};
+export default Footer;
